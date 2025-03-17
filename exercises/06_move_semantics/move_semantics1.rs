62a310a02686e2a38a59e1de02084cc99173208f
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-03-18 02:16:14 +0300

    notes on ownership and scope

--- exercises/06_move_semantics/move_semantics1.rs
+++ exercises/06_move_semantics/move_semantics1.rs
@@ -1,6 +1,6 @@
 // TODO: Fix the compiler error in this function.
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+    let mut vec = vec; // shadow vec, take ownership of its memory, make it mutable
 
     vec.push(88);
 
@@ -9,6 +9,11 @@ fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
 
 fn main() {
     // You can optionally experiment here.
+    let my_vec: Vec<i32> = vec![1, 2, 3];
+    let new_vec = fill_vec(my_vec); // new variable, take ownership of my_vec, push it out of scope
+    for element in new_vec {
+        println!("{element}");
+    }
 }
 
 #[cfg(test)]
