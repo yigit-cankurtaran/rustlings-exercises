cca1ff56a2ed0fb3f4a4eb12d816b9f34fa036bb
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-03-17 23:08:51 +0300

    better code for vecs1

--- exercises/05_vecs/vecs1.rs
+++ exercises/05_vecs/vecs1.rs
@@ -4,13 +4,19 @@ fn array_and_vec() -> ([i32; 4], Vec<i32>) {
     // TODO: Create a vector called `v` which contains the exact same elements as in the array `a`.
     // Use the vector macro.
     // let v = ???;
-    let v = vec![10, 20, 30, 40];
-
+    let v = a.to_vec(); // we just need this method
     (a, v)
 }
 
 fn main() {
     // You can optionally experiment here.
+    let (arr, vec) = array_and_vec();
+    for i in arr {
+        println!("{i}");
+    }
+    for i in vec {
+        println!("{i}");
+    }
 }
 
 #[cfg(test)]
