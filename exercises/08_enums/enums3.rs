8f44ddb0358d8dc0f59cc076f1d750cfa902e4a8
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2025-04-17 20:44:43 +0300

    finished enums

--- exercises/08_enums/enums3.rs
+++ exercises/08_enums/enums3.rs
@@ -46,6 +46,14 @@ impl State {
     fn process(&mut self, message: Message) {
         // TODO: Create a match expression to process the different message
         // variants using the methods defined above.
+
+        match message {
+            Message::Resize { width, height } => self.resize(width, height),
+            Message::Move(Point { x, y }) => self.move_position(Point { x, y }),
+            Message::Echo(s) => self.echo(s),
+            Message::ChangeColor(r, g, b) => self.change_color(r, g, b),
+            Message::Quit => self.quit(),
+        }
     }
 }
 
